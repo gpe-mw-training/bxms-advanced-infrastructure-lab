e6ae59ccba9cdad8d25f26f493c9c7b7bda000e0
Bernard Tison <bernard.tison@gmail.com>
2016-08-19 03:22:49 +0200

    changed port for RHSSO server

--- angularjs-ticket-app/scripts/config.js
+++ angularjs-ticket-app/scripts/config.js
@@ -2,10 +2,9 @@
 
 var ENV = { 
     'rhsso_host' : '127.0.0.1', 
-    'rhsso_port'  : '8080',
+    'rhsso_port'  : '8380',
     'rhsso_realm' : 'kieserver',
     'rhsso_clientId' : 'frontend',
-    'rhsso_secret' : '6e532c47-022c-47cc-a694-560dc585860b',
     'kieserver_host' : '127.0.0.1',
     'kieserver_port' : '8230',
     'kieserver_containerId' : 'ticket-app',
